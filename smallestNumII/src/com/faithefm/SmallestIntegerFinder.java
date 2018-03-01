91f852d5942380bf14b79cd67d357bec7ae2fc68
faithfem <fmugure@intracitygeeks.org>
2018-02-28 19:45:45 -0500

    smallestNumber Edabit

---
+++ smallestNumII/src/com/faithefm/SmallestIntegerFinder.java
@@ -0,0 +1,20 @@
+package com.faithefm;
+
+public class SmallestIntegerFinder {
+
+    public static int main(int[] args) {
+    //public static int findSmallestInt(int[] args){
+
+        int min = args[0];
+
+        for(int i = 0; i < args.length; i++){
+            if(args[i] <=min){
+                min = args[i];
+            }
+        }
+
+
+        return min;
+    }
+
+}

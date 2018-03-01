f4794151b8e1355d6fdc3dd021cc1ee6b7d4ee75
faithfem <fmugure@intracitygeeks.org>
2018-02-28 19:00:17 -0500

    smallestNumber Edabit

--- smallestNumber/src/com/faithefm/Main.java
+++ smallestNumber/src/com/faithefm/Main.java
@@ -1,10 +1,17 @@
 package com.faithefm;
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
 
-    
+        int[] numbers = {88, 33, 55, 23, 64, 123};
+        int smallest = Integer.MAX_VALUE;
 
+        for(int i = 0; i<numbers.length; i++){
+            if(smallest > numbers[i]){
+                smallest = numbers[i];
+            }
+        }
+        System.out.println("Smallest number in array is: " + smallest);
     }
 }

9c87beb0ef6610114d9000218883f4512d99c34c
faithfem <fmugure@intracitygeeks.org>
2018-03-18 17:50:43 -0400

    math

--- Math/src/Math.java
+++ Math/src/Math.java
@@ -1,6 +1,8 @@
 /*public class Math {
     public static void main(String[]args){
 
+        Quadratic_Equation Quadratic_Equation = new Quadratic_Equation();
+        Quadratic_Equation.QuadraticEquationMethod();
 
 
     }

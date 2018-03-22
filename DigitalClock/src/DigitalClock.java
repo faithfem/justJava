41dc390460c9a8f1e06a19dd820aee8fb29c9a71
faithfem <fmugure@intracitygeeks.org>
2018-03-22 19:38:50 -0400

    Digital Clock

--- DigitalClock/src/DigitalClock.java
+++ DigitalClock/src/DigitalClock.java
@@ -1,7 +1,25 @@
-public class DigitalClock {
+import javax.swing.*;
+import java.awt.*;
+
+public class DigitalClock extends JFrame {
+
+    JLabel jlabClock;
+
+    public DigitalClock(){
+        jlabClock = new JLabel("");
+        setLayout(new FlowLayout());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(jlabClock);
+        setSize(1200,120);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+
 
     public static void main(String[]args){
 
-        
+        new DigitalClock(); //method
+
     }
 }

aa7c687c872333e1bcc994f19573681ae472708b
faithfem <fmugure@intracitygeeks.org>
2018-03-25 14:12:21 -0400

    Digital Clock

---
+++ StdOutPut/src/com/faithefm/Main.java
@@ -0,0 +1,17 @@
+package com.faithefm;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a = 17;
+	int b = 23;
+
+	int sum = a + b;
+
+        System.out.println("Hello, World!");
+        System.out.printf("%d + %d = %d", a, b, sum);
+        //StdOut.println("Hello, Ulimwengu!");
+        //StdOut.printf("%d");
+
+    }
+}

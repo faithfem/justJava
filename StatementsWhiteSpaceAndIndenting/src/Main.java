c310da8213fc6c534dcb522fa70f80d6f975ad66
faithfem <fmugure@intracitygeeks.org>
2018-04-08 13:52:41 -0400

    Statements & White Space

---
+++ StatementsWhiteSpaceAndIndenting/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+
+    public static void main(String[]args){
+        int myVariable = 50;
+
+        if(myVariable==50){
+            System.out.println("Printed my variable:  " + myVariable);
+        }
+    }
+}
